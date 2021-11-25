e28e117a72de3c39250635d337404042bdca479a
Uriel <uriellopezaquino1.0@gmail.com>
2021-11-25 15:05:37 -0600

    creacion de fragments

--- app/src/main/java/com/example/snapshots/MainActivity.kt
+++ app/src/main/java/com/example/snapshots/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.snapshots
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.snapshots.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        nBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(nBinding.root)
+
+        setupBottomNav()
     }
+
+    private fun setupBottomNav(){
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().add(R.id.hostFragment, HomeFragment()).commit()
+    }
+
 }
